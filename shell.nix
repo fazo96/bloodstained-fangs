af9fbb79ec25ce8282e01664dc26bee8d1678116
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2025-12-05 23:02:58 +0100

    remove image resize script

--- shell.nix
+++ shell.nix
@@ -4,7 +4,6 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     typst # compiling the book
     libwebp # image compression
-    imagemagick # image manipulation
     netlify-cli # deploys
   ];
 }
